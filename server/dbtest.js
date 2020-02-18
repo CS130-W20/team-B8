7583a8191bc54631fb6391c29f027214571bec49
Ubuntu 1804 LTS <morgankie@hotmail.com>
2020-02-17 23:23:33 -0800

    Add sanity test for all db functions

---
+++ server/dbtest.js
@@ -0,0 +1,135 @@
+// DB Sanity Test
+// Run by calling `node dbtest.js`
+//Clean up db before rerunning
+
+var dbInterface = require('./dbInterface');
+var assert = require('assert');
+
+setTimeout(function(){
+    	console.log('startCalls');
+    	let user = {
+    		'name': 'a',
+		'email': 'b',
+		'password': 'c',
+		'interests': [],
+		'phone': '1234567891',
+		'eventsAttending': [],
+		'eventsHosting':[]
+	}
+	let u1 = dbInterface.addUser(user.name, user.email, user.password, user.phone).then(f =>{
+	 	dbInterface.getUser('a').then(r => {
+			delete r._id
+			assert.deepEqual(r, user);
+		});
+	});
+
+    	let u2 = u1.then( f => dbInterface.updateUserPassword(user.name , 'a').then(f =>{
+ 		dbInterface.getUser('a').then(r => {
+			assert.equal(r.password, 'a');
+			assert.equal(r.phone, user.phone);
+		});
+	}));
+	
+
+    	let u3 = u2.then( f => dbInterface.updateUserDetails(user.name, ['a'], user.phone))
+		.then(f => dbInterface.addUserAttendingEvent(user.name, 1))	
+		.then(f => dbInterface.addUserHostingEvent(user.name, 2))
+		.then(r => dbInterface.getUser('a'))
+		.then(r => {
+			assert.equal(r.interests[0], 'a');
+			assert.equal(r.interests.length, 1);
+			assert.equal(r.phone, user.phone);
+			assert.equal(r.eventsAttending[0], 1);
+			assert.equal(r.eventsAttending.length, 1);
+			assert.equal(r.eventsHosting[0], 2);
+			assert.equal(r.eventsHosting.length, 1);
+    	});
+    	let u4 = u3.then(f => dbInterface.removeUserAttendingEvent(user.name, 1))	
+		.then(f => dbInterface.removeUserHostingEvent(user.name, 2))
+		.then(r => dbInterface.getUser('a'))
+		.then(r => {
+			assert.equal(r.eventsAttending.length, 0);
+			assert.equal(r.eventsHosting.length, 0);
+    	});
+
+    	let event = {
+		'eventID': 1,
+		'title': 'foo',
+		'tag': ['tag'],
+		'location': 'loc',
+		'locationName': 'locNAme',
+		'host': 'h',
+		'attendees': [],
+		'reviews':[]
+   	 };
+    	let newEvent = {
+		'eventID': 1,
+		'title': 'bar',
+		'tag': ['tag'],
+		'location': 'loc',
+		'locationName': 'locName',
+		'host': 'h',
+		'attendees': ['a'],
+		'reviews':[{'user': 'a','score': 2,'review': 'c'}]
+    	};
+
+    	let e1 = dbInterface.addEvent(event.eventID, event.title, event.tag, event.location,event.locationName, event.host);
+    	let e2 = e1.then( f => dbInterface.getAllEvents())
+		.then( r =>{
+			let res = r[0];
+			assert.equal(res.eventID, event.eventID);
+			assert.equal(res.title, event.title);
+			assert.equal(res.tag[0], event.tag[0]);
+			assert.equal(res.location, event.location);
+			assert.equal(res.locationName, event.locationName);
+			assert.equal(res.host, event.host);
+			assert.equal(res.attendees.length, 0);
+			assert.equal(res.reviews.length, 0);
+
+    	});
+    	let e3 = e2.then( f => dbInterface.queryEvents())
+		.then(r => {
+			let res = r[0];
+			assert.equal(res.eventID, event.eventID);
+			assert.equal(res.title, event.title);
+			assert.equal(res.tag[0], event.tag[0]);
+			assert.equal(res.location, event.location);
+			assert.equal(res.locationName, event.locationName);
+			assert.equal(res.host, event.host);
+			assert.equal(res.attendees.length, 0);
+			assert.equal(res.reviews.length, 0);
+    	});
+    	let e4 = e3.then( f => dbInterface.getEvent(1))
+		.then(r => {
+			let res = r;
+			assert.equal(res.eventID, event.eventID);
+			assert.equal(res.title, event.title);
+			assert.equal(res.tag[0], event.tag[0]);
+			assert.equal(res.location, event.location);
+			assert.equal(res.locationName, event.locationName);
+			assert.equal(res.host, event.host);
+			assert.equal(res.attendees.length, 0);
+			assert.equal(res.reviews.length, 0);
+    	});
+
+    	let e5 = e4.then(f => dbInterface.updateEvent(newEvent.eventID , newEvent.title, newEvent.tag, newEvent.location, newEvent.locationName))
+		.then(r => dbInterface.addEventAttendee(newEvent.eventID, newEvent.attendees[0]))
+		.then(r => dbInterface.addEventReview(newEvent.eventID, newEvent.reviews[0].user, newEvent.reviews[0].score,newEvent.reviews[0].review))
+		.then(r => dbInterface.getEvent(newEvent.eventID))
+		.then(r => {
+			console.log(r);
+			assert.equal(r.title, newEvent.title);
+			assert.equal(r.tag[0], newEvent.tag[0]);
+			assert.equal(r.location, newEvent.location);
+			assert.equal(r.locationName, newEvent.locationName);
+			assert.equal(r.attendees[0], newEvent.attendees[0]);
+			assert.deepEqual(r.reviews[0], newEvent.reviews[0]);
+    	});
+	let e6 = e5.then(r => dbInterface.removeEventAttendee(newEvent.eventID, newEvent.attendees[0]))
+		.then(r => dbInterface.getEvent(newEvent.eventID))
+		.then(r => {
+			console.log(r);
+			assert.equal(r.attendees.length, 0);
+	});
+   	console.log('endCalls');
+}, 2000);
