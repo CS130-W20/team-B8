ba2cf5174a2180f7b3ae725896520cda31ce4e12
CaratCode <lawrencenlee@gmail.com>
2020-03-01 02:17:19 -0800

    added missing files

---
+++ src/mainUI/Events/EventListRow.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import EventEdit from './EditEvent';
+import EventDelete from './DeleteEvent';
+import EventMessage from './MessageEvent';
+
+
+export default class EventListRow extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const{
+            _id,
+            title,
+            timeDate,
+            locationName,
+            attendees,
+            tag,
+        } = this.props;
+
+        let dateObj = new Date(timeDate);
+        const time = dateObj.getHours() + ":" + dateObj.getMinutes();
+        const date = (dateObj.getMonth() + 1) + "-" + dateObj.getDate();
+        const location = locationName.slice(0, locationName.indexOf(','));
+        const numAttendees = attendees.length;
+
+        return (
+            <TableRow key={_id}>
+            <TableCell>{date}</TableCell>
+            <TableCell>{title}</TableCell>
+            <TableCell>{location}</TableCell>
+            <TableCell>{time}</TableCell>
+            <TableCell>{numAttendees}</TableCell>
+            <TableCell align="right" style={{display: 'grid', gridTemplateRows: '1fr', gridTemplateColumns: '1fr 1fr 1fr'}}>
+                <EventEdit title={title} date={timeDate} location={location} tag={tag}/>
+                <EventMessage/>
+                <EventDelete />
+            </TableCell>
+            </TableRow>
+        )
+    }
+}
---
+++ src/mainUI/Events/EventMarkers/BarMarker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Marker } from 'google-maps-react';
+import { render } from '@testing-library/react';
+import { getMarkerType, markerTypes } from '../../markerPrefab/mapMarker';
+
+export default class BarMarker extends React.Component{
+
+    render = () => {
+        return(
+            <Marker
+            onClick={this.props.handleClickOpen}
+            position={{ lat: this.props.location.lat, lng: this.props.location.lng}}
+            name={this.props.title}
+            icon={{url: getMarkerType(this.props.type), scaleSize: (.5, .5)}}/>
+        )        
+    }
+}
+// {
+            
+          //       return(
+          //         <Marker
+          //           onClick={this.handleClickOpen}
+          //           position={{ lat: marker.lat, lng: marker.lng}}
+          //           name={marker.name}
+          //           key={i}
+          //           icon={{url: marker.type, scaleSize: (.5, .5)}}/>
+          //       )
+          // })}
+
