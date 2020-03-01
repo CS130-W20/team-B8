875fdab6ad385125f0032b489dea240f1d12c11f
Phipson <phipsonleecy@gmail.com>
2020-03-01 13:42:55 -0800

    Merged more twilio and filter stuff with master

--- src/mainUI/login+registration/src/App/App.jsx
+++ src/mainUI/login+registration/src/App/App.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
-import { HomePage } from '../HomePage';
+import { HomePage } from '../HomePage/HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
 
