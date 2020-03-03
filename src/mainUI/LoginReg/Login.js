b8f8fc6122b46cdd795cacd5dfe1b41ca85bf2b8
candicezhang0827 <36454950+candicezhang0827@users.noreply.github.com>
2020-03-03 09:47:45 -0800

    Add files via upload

--- src/mainUI/LoginReg/Login.js
+++ src/mainUI/LoginReg/Login.js
@@ -10,10 +10,29 @@ import {
   LOGIN_MESSAGE,
   ERROR_IN_LOGIN,
 } from '../MessageBundle';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+//import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 
 //const io = require("socket.io-client"),
 //socket = io.connect("http://localhost:8000");
 //socket.emit('getAllEvents');
+
+
+
 
 export default class Login extends Component {
   constructor(props) {
@@ -70,59 +89,76 @@ export default class Login extends Component {
     });
   };
 
+
   render() {
     const { loginSuccess, error } = this.state;
+    const theme = createMuiTheme();  
+  return(
+    
+    <Container component="main" maxWidth="xs">
+    <h1></h1>
+      <CssBaseline />
+          <div marginTop={theme.spacing(8)}
+               display='flex'
+               flexDirection='column'
+               alignItems='cener'>
+        <Avatar margin={theme.spacing(1)}
+                backgroundColor={theme.palette.secondary.main}>
+          <LockOutlinedIcon/>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+            Sign in
+        </Typography>
+          <form width='100%'
+                marginTop={theme.spacing(1)} 
+                noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="Username"
+              label="Username"
+              name="Username"
+              autoFocus
+              onChange={this.handleOnChangeUserName}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={this.handleOnChangePassword}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={this.onSubmit}
+              margin={theme.spacing(3,0,2)}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link to="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+          {loginSuccess && <Message message={LOGIN_MESSAGE} />}
+          {error && <Error message={ERROR_IN_LOGIN} />}
+          </div>
+          
 
-    return (
-      <div className="Login">
-        <h1> {LOGIN_FIELDS.LOGIN_HEADING} </h1> {' '}
-        <form onSubmit={this.onSubmit}>
-          <div>
-            <div className="fields">
-              <p> {"Name"} </p>    {' '}
-              <input
-                type="text"
-                name="Username"
-                onChange={this.handleOnChangeUserName}
-                autoComplete="Username"
-                required
-              />
-            </div>{' '}
-            {' '}
-            <div className="fields">
-              {' '}
-              <p> {COMMON_FIELDS.PASSWORD} </p>    {' '}
-              <input
-                type="password"
-                name="Password"
-                onChange={this.handleOnChangePassword}
-                autoComplete="Password"
-                required
-              />{' '}
-                  {' '}
-            </div>{' '}
-            {' '}
-            <div className="buttons">
-              {' '}
-              <button
-                type="button"
-                onClick={this.onSubmit}
-                className="btn btn-primary"
-              >
-                {' '}
-                  {LOGIN_FIELDS.LOGIN}    {' '}
-              </button>{' '}
-                 <Link to="/register">
-                     {REGISTRATION_FIELDS.REGISTER} </Link>  {' '}
-               {' '}
-            </div>{' '}
-               {' '}
-          </div>{' '}
-           {' '}
-        </form>{' '}
-            {loginSuccess && <Message message={LOGIN_MESSAGE} />}    {' '}
-        {error && <Error message={ERROR_IN_LOGIN} />}    {' '}
-      </div>
+      </Container>
+     
     );
   }
 }
